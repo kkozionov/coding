31919715162f8038e95df01553f1f99dc0493f0b
Kirill Kozionov <kirill@sympli.io>
2020-06-23 22:56:10 +0400

    [*] Comments

--- leetcode/2020/06/23/bitwise-and/Solution.java
+++ leetcode/2020/06/23/bitwise-and/Solution.java
@@ -1,3 +1,7 @@
+/**
+ * 201. Bitwise AND of Numbers Range
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.s
+ */
 class Solution {
     public int rangeBitwiseAnd(int m, int n) {
         int shifts = 0;
