3f6b342253d75a4dea6fe55a63c5731f275379f8
Kirill Kozionov <kirill@sympli.io>
2020-06-24 00:07:38 +0400

    [*] Comments

--- src/com/kozionov/leetcode/easy/tree/SymmetricTree.java
+++ src/com/kozionov/leetcode/easy/tree/SymmetricTree.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
+ * 101. Symmetric Tree
  * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
  *
  * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
